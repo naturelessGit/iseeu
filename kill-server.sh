e67177de524477c9fa12ab3b34296312adb6fdf3
Maiake-ops <three.dev7083@gmail.com>
2025-06-03 13:47:46 +0700

    Update kill-server.sh

--- kill-server.sh
+++ kill-server.sh
@@ -10,7 +10,7 @@ echo "[*] Killing Python HTTP server..."
 pkill -f "python3 -m http.server"
 
 echo "[*] Killing ncat..."
-pkill ncat
+pkill ncat && lsof -i :8080
 
 echo "[*] services killed successfully" && sleep 5
 chmod +x start.sh && ./start.sh
